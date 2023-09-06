74fe8f0c7a1d2724a459f2029ef5e5b688c08a8c
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-09-06 13:54:10 -0500

    Update view-header.php

--- view-header.php
+++ view-header.php
@@ -8,7 +8,6 @@
   </head>
   <body>
     <div class="container">
-
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
