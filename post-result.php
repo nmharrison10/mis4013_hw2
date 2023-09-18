0dcb6611e532bda15219cd06cf47c38c5c3e638c
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-09-17 22:55:47 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -1,22 +1,21 @@
 <?php
-$pagetitle = "Post Results";
+$pageTitle = "Post Results";
 include "view-header.php";
-
 ?>
-
 <h1>Post Results</h1>
-
 <?php
 echo getDisplay;
 include "view-footer.php";
 
-  function getDisplay() {
-    if (isset($_POST["my_name"])) {
-  return "The value sent is:" : $_POST["my_name"];
-  }
-else {
-  return "Nothing posted to this page.";
+if (isset($_POST['my_name'])) {
+  ?>
+  <p>The value sent is: </p>
+<?php
+$_POST['my_name'];
+} else {
+  ?>
+  <p>Nothing posted to this page.</p>
+<?php
 }
-  }
-  
+include "view-footer.php";
 ?>
