63e977d36ff3e329d2811939d89b882dec5fd744
nmharrison10 <122953873+nmharrison10@users.noreply.github.com>
2023-09-17 23:03:42 -0500

    Update post-result.php

--- post-result.php
+++ post-result.php
@@ -5,12 +5,12 @@ include "view-header.php";
 <h1>Post Results</h1>
 <?php
 if (isset($_POST['my_name'])) {
-  ?>
+?>
   <p>The value sent is: </p>
 <?php
-$_POST['my_name'];
+ echo $_POST['my_name'];
 } else {
-  ?>
+?>
   <p>Nothing posted to this page.</p>
 <?php
 }
